80b392267a08e8e9c8cd0aa48f549f229bb99962
prakash-rahangdale <prakash170817@gmail.com>
2021-04-11 23:23:56 +0530

    showing a loading spinner and add some styling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import "semantic-ui-css/semantic.min.css";
+import Spinner from './Spinner';
 
 class App extends React.Component {
     state = { lat: null, errorMessage: '' };
@@ -23,7 +24,7 @@ class App extends React.Component {
             return <SeasonDisplay lat = {this.state.lat} />
         }
 
-        return <div>loading</div>;
+        return <Spinner />;
     }
 }
 
